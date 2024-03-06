f42e8ae8344ca6d8b52c2718cd52cdec3bb4b58c
diegofloriano <slrerfloriano@gmail.com>
2024-03-06 20:48:14 -0300

    classes

---
+++ Lista2_Exercicios_Classes/src/index.ts
@@ -0,0 +1,31 @@
+//1- 
+class Carro{
+    private data: Date;
+    constructor(nome:string, idade:number,matricula:string,turma:string){
+        this.data =data;
+    }
+}
+
+//2-
+class Calculadora{
+    private valor1: number;
+    private valor2: number;
+    constructor(nome:string, idade:number,matricula:string,turma:string){
+        this.valor1 =nvalor1;
+        this.valor2 = alor2;
+    }
+      
+}
+
+//3-
+class Produto{
+    private nome: string;
+    private preço: number;
+    private QuantidadeEmEstoque: number;
+    constructor(nome:string, idade:number,matricula:string,turma:string){
+        this.nome =nome;
+        this.preço = preço;
+        this.QuantidadeEmEstoque = QuantidadeEmEstoque;
+       
+}
+
