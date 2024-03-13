e84c480e5474cbe0d910bd07b99179dbe5048d0a
diegofloriano <slrerfloriano@gmail.com>
2024-03-13 20:33:54 -0300

    aula 7

---
+++ Aula_7_Interfaces/src/Zoologico/index.ts
@@ -0,0 +1,9 @@
+import {Animal} from "./Animal"
+import {Leao} from "./Animal"
+import {Pato} from "./Animal"
+
+
+function imprimeAnimal(animal: Animal){
+    console.log(`O animal ${animal.name} eh mamifero :${animal.mamifero}`);
+    console.log(`O animal ${animal.name} eh mamifero :${animal.mamifero}`)
+}
