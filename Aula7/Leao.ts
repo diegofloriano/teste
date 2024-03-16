4d8f5aab714edb9f5736d08d3deef8856d5c9af5
diegofloriano <slrerfloriano@gmail.com>
2024-03-15 22:31:15 -0300

    animal

--- Aula7/Leao.ts
+++ Aula7/Leao.ts
@@ -1,18 +1,11 @@
-import {Animal} from "./Animal"
-
+import { Animal } from "./Animal";
 export class Leao implements Animal{
     name: string;
-
-    constructor(name:string){
-        this.name = name;
-    }
     mamifero(): boolean {
         return true;
-
     }
     ave(): boolean {
         return false;
-
     }
-
+    
 }
