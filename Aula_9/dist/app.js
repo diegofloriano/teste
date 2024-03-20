7e80d6c99d516a5590dc03c25d646afbb3f98055
diegofloriano <slrerfloriano@gmail.com>
2024-03-20 20:46:50 -0300

    aula9

---
+++ Aula_9/dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Hello World");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+function appLog() {
+    console.log("A API está disponivel no URL http://localhost:3000");
+}
+app.listen(PORT, appLog);
+function hello(resq, res) {
+    return res.send("Hello World!!");
+}
+app.get("/api/hello", hello);
+app.listen(PORT, appLog);
